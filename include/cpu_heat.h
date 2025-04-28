7943b3547360391a0a1b23dfe83a7f4fc6e293ed
Poppy-sun <lishuangsun50@gmail.com>
2025-04-28 11:48:13 +0000

    Add CPU header file with function declarations for heat simulation

--- include/cpu_heat.h
+++ include/cpu_heat.h
@@ -1,6 +1,12 @@
 #ifndef CPU_HEAT_H
 #define CPU_HEAT_H
 
+#include <vector>
+
+void initialize_matrices(std::vector<std::vector<float>>&, std::vector<std::vector<float>>&, int, int);
+void heat_step(const std::vector<std::vector<float>>&, std::vector<std::vector<float>>&, int, int);
+void compute_row_averages(const std::vector<std::vector<float>>&, int, int);
+
 void run_cpu_simulation(int n, int m, int p, bool compute_average);
 
 #endif // CPU_HEAT_H
