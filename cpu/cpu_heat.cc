0f2ab68a2eaf8f53c8fc6cb8bb2b28fad5bd81ec
Poppy-sun <lishuangsun50@gmail.com>
2025-04-28 11:53:26 +0000

    Implement matrix initialization with boundary conditions

--- cpu/cpu_heat.cc
+++ cpu/cpu_heat.cc
@@ -7,8 +7,20 @@
 // Function to initialize the matrices
 void initialize_matrices(std::vector<std::vector<float>>& previousMatrix,
                          std::vector<std::vector<float>>& nextMatrix,
-                         int n, int m) {
-    }
+   			 int n, int m) {
+// Initialize both matrices
+    for (int i = 0; i < n; ++i) {
+        float boundary_value = 0.98f * (float)((i + 1) * (i + 1)) / (float)(n * n);
+        previousMatrix[i][0] = boundary_value;
+        nextMatrix[i][0] = boundary_value;
+
+        for (int j = 1; j < m; ++j) {
+            float init_value = boundary_value * (float)((m - j) * (m - j)) / (float)(m * m);
+            previousMatrix[i][j] = init_value;
+            nextMatrix[i][j] = init_value;
+        }
+    }   
+}
 
 // Function to perform one heat propagation step
 void heat_step(const std::vector<std::vector<float>>& previousMatrix,
